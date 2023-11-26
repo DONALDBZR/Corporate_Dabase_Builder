653ce741c766e01dcda92fbda69e73a186d79145
DONALDBZR <andygaspard@hotmail.com>
2023-11-26 11:07:27 +0400

    [UPDATE 0.4.13] Fix: Log Message: mysql.connector.errors.InterfaceError: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '00:00:00 AND CONCAT(2023, '-', end_date) > 2023-11-25 00:00:00'

--- Classes/Builder.py
+++ Classes/Builder.py
@@ -81,8 +81,8 @@ class Builder:
         data: tuple[int, str, str, str] = self.getDatabaseHandler().get_data(
             parameters=filters,
             table_name="FinancialCalendar",
-            filter_condition=f"CONCAT({str(self.getDate().year)}, '-', start_date) < {str(self.getDate())} AND CONCAT({str(self.getDate().year)}, '-', end_date) > {str(self.getDate())}",
-            column_names=f"{str(self.getDate().year)} AS year, quarter, CONCAT({str(self.getDate().year)}, '-', start_date) AS start_date, CONCAT({str(self.getDate().year)}, '-', end_date) AS end_date"
+            filter_condition=f"CONCAT(YEAR({self.getDate()}), '-', start_date) < {self.getDate()} AND CONCAT(YEAR({self.getDate()}), '-', end_date) > {self.getDate()}",
+            column_names=f"YEAR({self.getDate()}) AS year, quarter, CONCAT(YEAR({self.getDate()}), '-', start_date) AS start_date, CONCAT(YEAR({self.getDate()}), '-', end_date) AS end_date"
         )[0]
         quarter = {
             "year": int(data[0]),
