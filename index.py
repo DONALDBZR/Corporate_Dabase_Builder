7eb9363660f7e8792347735ab8a998fb8413f87d
DONALDBZR <andygaspard@hotmail.com>
2023-11-26 13:59:03 +0400

    [UPDATE 0.4.39] Optimizing the application

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from Classes.Builder import Builder
 
-
-Corporate_Database_Builder = Builder("2023-11-25")
+Corporate_Database_Builder = Builder(str(input("Enter a date to retrieve the corporate metadata for the quarter of that date")))
 """
 The builder which will build the database.
 """
