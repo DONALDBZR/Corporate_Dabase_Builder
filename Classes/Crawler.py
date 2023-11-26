46d47ce79aad752d01eb1bad5710b1cff0166042
DONALDBZR <andygaspard@hotmail.com>
2023-11-26 19:13:07 +0400

    [UPDATE 0.5.8] Retrieving corporate metadata about the companies that operate in Mauritius.

--- Classes/Crawler.py
+++ Classes/Crawler.py
@@ -216,14 +216,13 @@ class Crawler:
         ).text.replace("1 – 10 of ", "")
         print(f"Data Amount: {data_amount}")
         amount = int(data_amount)
-        print(amount)
-        # self.getLogger().inform(
-        #     f"Search completed for corporate metadata between {date_from} and {date_to}\nDate From: {date_from}, Date To: {date_to}\nAmount: {amount}"
-        # )
-        # self.setHtmlTag(
-        #     self.getDriver().find_element(
-        #         By.XPATH,
-        #         f"{self.ENV.getTargetApplicationRootXpath()}/cbris-search-results/lib-mns-universal-table/div/div[1]/table/tbody"
-        #     )
-        # )
+        self.getLogger().inform(
+            f"Search completed for corporate metadata between {date_from} and {date_to}\nDate From: {date_from}, Date To: {date_to}\nAmount: {amount}"
+        )
+        self.setHtmlTag(
+            self.getDriver().find_element(
+                By.XPATH,
+                f"{self.ENV.getTargetApplicationRootXpath()}/cbris-search-results/lib-mns-universal-table/div/div[1]/table/tbody"
+            )
+        )
         return response
