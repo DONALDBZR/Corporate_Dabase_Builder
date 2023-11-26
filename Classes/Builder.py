756dd229a9b442b79071e9ebdfa21c791cd0ecf8
DONALDBZR <andygaspard@hotmail.com>
2023-11-26 13:02:28 +0400

    [UPDATE 0.4.30] Validating the quarter against to be able to date to know that the Crawler must take for a quarter that is before the current quarter.

--- Classes/Builder.py
+++ Classes/Builder.py
@@ -83,10 +83,40 @@ class Builder:
             filter_condition=f"CONCAT(YEAR('{str(self.getDate().date())}'), '-', start_date) < '{str(self.getDate().date())}' AND CONCAT(YEAR('{str(self.getDate().date())}'), '-', end_date) > '{str(self.getDate().date())}'",
             column_names=f"YEAR('{str(self.getDate().date())}') AS year, quarter, FROM_UNIXTIME(UNIX_TIMESTAMP(CONCAT(YEAR('{str(self.getDate().date())}'), '-', start_date)), '%m/%d/%Y') AS start_date, FROM_UNIXTIME(UNIX_TIMESTAMP(CONCAT(YEAR('{str(self.getDate().date())}'), '-', end_date)), '%m/%d/%Y') AS end_date"
         )[0]
-        quarter = {
+        quarter: dict[str, int | str] = {
             "year": int(data[0]),
             "quarter": str(data[1]),
             "start_date": str(data[2]),
             "end_date": str(data[3])
         }
+        self.validateFinancialCalendarEndDate(quarter, self.getDate())
+        response = self.getCrawler().retrieveCorporateMetadata(quarter["start_date"], quarter["end_date"])
         print(quarter)
+
+    def validateFinancialCalendarEndDate(self, quarter: dict[str, int | str], date: datetime) -> dict:
+        """
+        Validating the quarter against to be able to date to know
+        that the Crawler must take for a quarter that is before the
+        current quarter.
+
+        Parameters:
+            quarter:    (object):   The quarter to be used as parameter for the search
+            date:       (Datetime): The date entered by the user.
+
+        Return:
+            (object)
+        """
+        response: dict
+        end_date = datetime.timestamp(datetime.strptime(str(quarter["end_date"]), "%m/%d/%Y"))
+        date_entered = datetime.timestamp(date)
+        if date_entered > end_date:
+            response = {
+                "status": 401,
+                "message": "Data from the current quarter cannot be taken!"
+            }
+        else:
+            response = {
+                "status": 200,
+                "message": f"Data from the quarter {quarter['year']} {quarter['quarter']} can be taken"
+            }
+        return response
