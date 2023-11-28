54aa1fd7510587cc792e51a000066c6b9aa37f43
DONALDBZR <andygaspard@hotmail.com>
2023-11-28 23:40:05 +0400

    [UPDATE 0.7.20] Retrieving corporate metadata about the companies that operate in Mauritius.

--- Classes/Crawler.py
+++ Classes/Crawler.py
@@ -197,7 +197,7 @@ class Crawler:
         Return:
             (object)
         """
-        delay: float = ((self.ENV.calculateDelay(date_from) + self.ENV.calculateDelay(date_to)) / 2) * (1.1 ** 7)
+        delay: float = ((self.ENV.calculateDelay(date_from) + self.ENV.calculateDelay(date_to)) / 2) * (1.1 ** 8)
         amount: int
         response: dict = {}
         print(f"Delay: {delay}s")
