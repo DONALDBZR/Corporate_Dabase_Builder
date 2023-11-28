ffb96df801e8b9e8f38f4af0f2ad4c411da856a4
DONALDBZR <andygaspard@hotmail.com>
2023-11-28 22:59:09 +0400

    [UPDATE 0.7.12] Refreshing the data up to the latest data that the crawler has taken.

--- Classes/Crawler.py
+++ Classes/Crawler.py
@@ -274,7 +274,7 @@ class Crawler:
                     self.getDriver(),
                     delay
                 ).until(
-                    expected_conditions.visibility_of_element_located(
+                    expected_conditions.element_to_be_clickable(
                         (
                             By.XPATH,
                             f"{self.ENV.getTargetApplicationRootXpath()}/cbris-search-results/lib-mns-universal-table/div/div[2]/mat-paginator/div/div/div[2]/button[3]"
