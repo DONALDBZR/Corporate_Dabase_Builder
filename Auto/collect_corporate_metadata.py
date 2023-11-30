d27f0275b207a3d46a90729a35c19c85077c2c12
DONALDBZR <andygaspard@hotmail.com>
2023-11-30 19:09:40 +0400

    [UPDATE 0.8.6] The script will be the first module of the application.  It will be run automatically by using CronTab.

---
+++ Auto/collect_corporate_metadata.py
@@ -0,0 +1,7 @@
+from Classes.Builder import Builder
+
+Corporate_Database_Builder = Builder()
+"""
+The builder which will build the database.
+"""
+Corporate_Database_Builder.collectCorporateMetadata()
