6cb952fa4de2d1b7bb63d6d8cb245fb0a5ff68ee
Andy Ewen Gaspard <andyg@finclub.mu>
2023-12-01 15:49:12 +0400

    [UPDATE 0.9.16] Pointing towards the correct directory

--- Auto/fincorp_module1.sh
+++ Auto/fincorp_module1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
-python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/collect_corporate_metadata.py
-# rm -r /home/darkness4869/Documents/Corporate_Database_Builder/Cache/*
+source /home/devfc/Documents/FinCorp/venv/bin/activate
+python3 /home/devfc/Documents/FinCorp/Auto/collect_corporate_metadata.py
+# rm -r /home/devfc/Documents/FinCorp/Cache/*
