1aee1a363ff3a641ee7eca85da124d3b258d4289
DONALDBZR <andygaspard@hotmail.com>
2023-12-01 07:40:57 +0400

    [UPDATE 0.9.13] Driver script

--- Auto/fincorp_module1.sh
+++ Auto/fincorp_module1.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
 python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/collect_corporate_metadata.py
 rm -r /home/darkness4869/Documents/Corporate_Database_Builder/Cache/*
