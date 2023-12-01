c918a402cc237b3c54fdac4a02ba437a7a79172f
DONALDBZR <andygaspard@hotmail.com>
2023-12-01 04:23:45 +0400

    [UPDATE 1.0.0]

--- Auto/fincorp_module1.sh
+++ Auto/fincorp_module1.sh
@@ -1,3 +1,3 @@
-source ./venv/bin/activate
-python3 ./Auto/collect_corporate_metadata.py
-rm -r ./Cache/*
+source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
+python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/collect_corporate_metadata.py
+rm -r /home/darkness4869/Documents/Corporate_Database_Builder/Cache/*
