1ae5ba399354fcd986ab7f3d2fb59f8261545d14
DONALDBZR <andygaspard@hotmail.com>
2023-12-03 18:32:40 +0400

    [UPDATE 1.0.12] module driver file

--- Auto/collect_corporate_metadata.py
+++ Auto/collect_corporate_metadata.py
@@ -1,7 +1,10 @@
 from Classes.Builder import Builder
+from datetime import datetime
+from datetime import timedelta
 
-Corporate_Database_Builder = Builder()
-"""
-The builder which will build the database.
-"""
-Corporate_Database_Builder.collectCorporateMetadata()
+
+module_limit_execution = datetime.now() + timedelta(minutes=15)
+module_limit_execution_time = module_limit_execution.timestamp()
+while datetime.now().timestamp() < module_limit_execution_time:
+    Corporate_Database_Builder = Builder()
+    Corporate_Database_Builder.collectCorporateMetadata()
