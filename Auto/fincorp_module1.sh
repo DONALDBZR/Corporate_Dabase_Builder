ec7e806b5b7805784e1240870bd6ce7183341204
JyotikaR <jyotikar@finclub.mu>
2024-05-09 17:20:18 +0400

    [UPDATE 1.0.30] Server change due to Memory Issues

--- Auto/fincorp_module1.sh
+++ Auto/fincorp_module1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/devfc/Documents/fincorp/venv/bin/activate
-python3 /home/devfc/Documents/fincorp/Auto/collect_corporate_metadata.py
+source /home/admin/Documents/Darkness4869/fincorp/venv/bin/activate
+python3 /home/admin/Documents/Darkness4869/fincorp/Auto/collect_corporate_metadata.py
 # rm -r /home/devfc/Documents/fincorp/Cache/*
