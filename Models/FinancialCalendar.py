43d6925143b23fdd90199352504b0032ccbbc8ec
Andy Ewen Gaspard <andyg@finclub.mu>
2024-05-09 12:08:28 +0400

    [UPDATE 1.0.15] Retrieving the current financial quarter.

--- Models/FinancialCalendar.py
+++ Models/FinancialCalendar.py
@@ -12,6 +12,7 @@ from Models.DatabaseHandler import Database_Handler
 from Data.FinancialCalendar import FinancialCalendar
 from typing import Union, Dict
 from mysql.connector.types import RowType
+from mysql.connector.errors import Error
 
 
 class Financial_Calendar(Database_Handler):
@@ -40,15 +41,25 @@ class Financial_Calendar(Database_Handler):
     def setTableName(self, table_name: str) -> None:
         self.__table_name = table_name
 
-    def getCurrentQuarter(self) -> FinancialCalendar:
+    def getCurrentQuarter(self) -> Union[FinancialCalendar, None]:
         """
         Retrieving the current financial quarter.
 
         Returns:
-            FinancialCalendar
+            FinancialCalendar | null
         """
-        data: Union[RowType, Dict[str, Union[int, str]]] = self.getData(
-            table_name=self.getTableName(),
-            filter_condition="CONCAT(YEAR(CURDATE()), '-', start_date) < CURDATE() AND CONCAT(YEAR(CURDATE()), '-', end_date) > CURDATE()",
-            column_names="YEAR(CURDATE()) AS year, quarter, FROM_UNIXTIME(UNIX_TIMESTAMP(CONCAT(YEAR(CURDATE()), '-', start_date)), '%m/%d/%Y') AS start_date, FROM_UNIXTIME(UNIX_TIMESTAMP(CONCAT(YEAR(CURDATE()), '-', end_date)), '%m/%d/%Y') AS end_date"
-        )[0]
+        try:
+            data: Union[RowType, Dict[str, Union[int, str]]] = self.getData(
+                table_name=self.getTableName(),
+                filter_condition="CONCAT(YEAR(CURDATE()), '-', start_date) < CURDATE() AND CONCAT(YEAR(CURDATE()), '-', end_date) > CURDATE()",
+                column_names="YEAR(CURDATE()) AS year, quarter, FROM_UNIXTIME(UNIX_TIMESTAMP(CONCAT(YEAR(CURDATE()), '-', start_date)), '%m/%d/%Y') AS start_date, FROM_UNIXTIME(UNIX_TIMESTAMP(CONCAT(YEAR(CURDATE()), '-', end_date)), '%m/%d/%Y') AS end_date"
+            )[0]
+            self.getLogger().inform(
+                f"The data from {self.getTableName()} has been retrieved!\nStatus: 200\nData: {data}"
+            )
+            return FinancialCalendar(data)
+        except Error as error:
+            self.getLogger().error(
+                f"An error occurred in {self.getTableName()}\nStatus: 503\nError: {error}"
+            )
+            return None
