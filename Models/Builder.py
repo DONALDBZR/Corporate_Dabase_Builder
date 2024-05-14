1bc8e20930475be677acbeda5f63953adb7a21b0
DONALDBZR <andygaspard@hotmail.com>
2024-05-15 01:00:45 +0400

    [UPDATE 1.0.47] Retrieving the date end from the log data and comparing to retrieving the latest one.

--- Models/Builder.py
+++ Models/Builder.py
@@ -176,6 +176,23 @@ class Builder:
                 f"{self.ENV.getDirectory()}/Cache/CorporateDataCollection/{files[index]}"
             )
 
+    def _getDateEndFinCorpLogs(self, log: FinCorpLogs, date_end: int) -> int:
+        """
+        Retrieving the date end from the log data and comparing to
+        retrieving the latest one.
+
+        Parameters:
+            log: FinCorpLogs
+            date_end: int
+
+        Returns:
+            int
+        """
+        if log.date_to > date_end:
+            return log.date_to
+        else:
+            return date_end
+
     def getDateStart(self, logs: List[FinCorpLogs]) -> str:
         """
         Retrieving the next start date for the data collection which
@@ -190,10 +207,6 @@ class Builder:
         date_end: int = 0
         for index in range(0, len(logs), 1):
             date_end = self._getDateEndFinCorpLogs(logs[index], date_end)
-            if logs[index].date_to > date_end:
-                date_end = logs[index].date_to
-            else:
-                date_end = date_end
         return datetime.strftime(
             datetime.strptime(
                 datetime.fromtimestamp(date_end).strftime("%m/%d/%Y"),
