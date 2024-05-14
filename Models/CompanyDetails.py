815c0b37ad19535af08f1142fb6f82d37a0c8cac
DONALDBZR <andygaspard@hotmail.com>
2024-05-15 01:39:52 +0400

    [UPDATE 1.0.61] Adding the compony metadata into the relational database server.

--- Models/CompanyDetails.py
+++ Models/CompanyDetails.py
@@ -41,90 +41,10 @@ class Company_Details(Database_Handler):
     def setTableName(self, table_name: str) -> None:
         self.__table_name = table_name
 
-    def getSuccessfulRunsLogs(self) -> List[FinCorpLogs]:
+    def addCompany(self, data: Tuple[Any]) -> None:
         """
-        Retrieving the list of all successful runs.
-
-        Returns:
-            array
-        """
-        try:
-            data: Union[List[RowType], List[Dict[str, Union[int, str]]]] = self.getData(
-                table_name=self.getTableName(),
-                parameters=None,
-                filter_condition="status = 200"
-            )
-            response: Dict[str, Union[int, List[FinCorpLogs]]
-                           ] = self._getSuccessfulLogs(data)
-            self.getLogger().inform(
-                f"The data from {self.getTableName()} has been retrieved!\nStatus: {response['status']}\nData: {data}"
-            )
-            return response["data"]  # type: ignore
-        except Error as error:
-            self.getLogger().error(
-                f"An error occurred in {self.getTableName()}\nStatus: 503\nError: {error}"
-            )
-            return []
-
-    def _getSuccessfulLogs(self, dataset: Union[List[RowType], List[Dict[str, Union[int, str]]]]) -> Dict[str, Union[int, List[FinCorpLogs]]]:
-        """
-        Retrieving the data into the correct data type for the
-        application.
-
-        Parameters:
-            data: array
-
-        Returns:
-            {status: int, data: array}
-        """
-        succesful_logs: Dict[str, Union[int, List[FinCorpLogs]]]
-        if len(dataset) > 0:
-            succesful_logs = self.__getSuccessfulLogs(dataset)
-        else:
-            succesful_logs = {
-                "status": 204,
-                "data": [
-                    FinCorpLogs({
-                        "identifier": None,
-                        "method_name": None,
-                        "year": None,
-                        "quarter": None,
-                        "date_start": None,
-                        "date_to": None,
-                        "status": None,
-                        "amount": None,
-                    })
-                ]
-            }
-        return {
-            "status": succesful_logs["status"],
-            "data": succesful_logs["data"]
-        }
-
-    def __getSuccessfulLogs(self, dataset: Union[List[RowType], List[Dict[str, Union[int, str]]]]) -> Dict[str, Union[int, List[FinCorpLogs]]]:
-        """
-        Formating the data into the correct datatype when the result
-        set is not empty.
-
-        Parameters:
-            dataset: array
-
-        Returns:
-            {status: int, data: array}
-        """
-        status: int = 200
-        data: List[FinCorpLogs] = []
-        for index in range(0, len(dataset), 1):
-            data.append(FinCorpLogs(dataset[index]))  # type: ignore
-        return {
-            "status": status,
-            "data": data
-        }
-
-    def postSuccessfulCorporateDataCollectionRun(self, data: Tuple[Any]) -> None:
-        """
-        Inserting the successful run for the corporate data
-        collection.
+        Adding the compony metadata into the relational database
+        server.
 
         Parameters:
             data: array
@@ -134,24 +54,7 @@ class Company_Details(Database_Handler):
         """
         return self.postData(
             table=self.getTableName(),
-            columns="method_name, quarter, date_start, date_to, status, amount, amount_found",
-            values="%s, %s, %s, %s, %s, %s, %s",
-            parameters=data
-        )
-
-    def postFailedCorporateDataCollectionRun(self, data: Tuple[Any]) -> None:
-        """
-        Inserting the failure run for the corporate data collection.
-
-        Parameters:
-            data: array
-
-        Returns:
-            void
-        """
-        return self.postData(
-            table=self.getTableName(),
-            columns="method_name, quarter, date_start, date_to, status, amount, amount_found",
-            values="%s, %s, %s, %s, %s, %s, %s",
-            parameters=data
+            parameters=data,
+            columns="name, file_number, category, date_incorporation, nature, status",
+            values="%s, %s, %s, %s, %s, %s"
         )
