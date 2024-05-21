99999bf0857fd7e3923e6c4c2ab28be0fef119cc
DONALDBZR <andygaspard@hotmail.com>
2024-05-21 22:53:46 +0400

    [UPDATE 1.3.34] 7 Hours Testing

--- Auto/collect_corporate_metadata.py
+++ Auto/collect_corporate_metadata.py
@@ -1,3 +1,4 @@
+from Models.Builder import Builder
 from datetime import datetime
 from datetime import timedelta
 from sys import path
@@ -6,10 +7,7 @@ from sys import path
 path.insert(0, "/home/darkness4869/Documents/Corporate_Database_Builder")
 
 
-from Models.Builder import Builder
-
-
-module_limit_execution = datetime.now() + timedelta(minutes=15)
+module_limit_execution = datetime.now() + timedelta(hours=7)
 module_limit_execution_time = module_limit_execution.timestamp()
 while datetime.now().timestamp() < module_limit_execution_time:
     Corporate_Database_Builder = Builder()
