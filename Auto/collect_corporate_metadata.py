50232632eefaa9e012047a8f98b091b12cd33b2c
DONALDBZR <andygaspard@hotmail.com>
2024-05-22 08:25:03 +0400

    [UPDATE 1.3.34] Reseting the operational time execution of the Module

--- Auto/collect_corporate_metadata.py
+++ Auto/collect_corporate_metadata.py
@@ -1,4 +1,3 @@
-from Models.Builder import Builder
 from datetime import datetime
 from datetime import timedelta
 from sys import path
@@ -7,7 +6,10 @@ from sys import path
 path.insert(0, "/home/darkness4869/Documents/Corporate_Database_Builder")
 
 
-module_limit_execution = datetime.now() + timedelta(hours=7)
+from Models.Builder import Builder
+
+
+module_limit_execution = datetime.now() + timedelta(minutes=15)
 module_limit_execution_time = module_limit_execution.timestamp()
 while datetime.now().timestamp() < module_limit_execution_time:
     Corporate_Database_Builder = Builder()
