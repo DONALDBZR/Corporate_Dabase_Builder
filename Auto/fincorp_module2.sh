3becc2469447f87f39c49d5ca16e245294f0c40e
DONALDBZR <andygaspard@hotmail.com>
2024-06-16 16:20:35 +0400

    [UPDATE 1.4.1] The main script for retrieving the corporate file which contains all of the data about the company needed to build the corporate database.

---
+++ Auto/fincorp_module2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
+python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/download_corporate_file.py
