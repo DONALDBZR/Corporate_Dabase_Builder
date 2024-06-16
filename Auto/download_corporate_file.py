b4da372bf4432b207e46b346c67cf9a9c69a1085
DONALDBZR <andygaspard@hotmail.com>
2024-06-16 16:38:04 +0400

    [UPDATE 1.4.3] The python script for downloading the corporate file which contains the data about the company

---
+++ Auto/download_corporate_file.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from datetime import timedelta
+from sys import path
+
+
+path.insert(0, "/home/darkness4869/Documents/Corporate_Database_Builder")
+
+
+from Models.Builder import Builder
+
+
+module_limit_execution = datetime.now() + timedelta(minutes=15)
+module_limit_execution_time = module_limit_execution.timestamp()
+while datetime.now().timestamp() < module_limit_execution_time:
+    Corporate_Database_Builder = Builder()
+    Corporate_Database_Builder.downloadCorporateFile()
