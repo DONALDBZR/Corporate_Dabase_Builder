deca8d9c8b142644bdd52b13656a1195613cb4f0
DONALDBZR <andygaspard@hotmail.com>
2024-06-17 15:12:31 +0400

    [UPDATE 1.4.42] Downloading the file from the targeted application in order to store it into the relational database server.

--- Models/Crawler.py
+++ Models/Crawler.py
@@ -27,6 +27,7 @@ import logging
 import os
 import json
 import random
+import base64
 
 
 class Crawler:
@@ -442,6 +443,25 @@ class Crawler:
             file = self.downloadFile()
             print(f"Data: {data}")
             exit()
+
+    def downloadFile(self) -> bytes:
+        """
+        Downloading the file from the targeted application in order
+        to store it into the relational database server.
+
+        Returns:
+            bytes
+        """
+        response: int | str = self.getDriver().execute_async_script(
+            "var uri = arguments[0]; var callback = arguments[1]; var toBase64 = function(buffer){for(var r,n=new Uint8Array(buffer),t=n.length,a=new Uint8Array(4*Math.ceil(t/3)),i=new Uint8Array(64),o=0,c=0;64>c;++c)i[c]='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.charCodeAt(c);for(c=0;t-t%3>c;c+=3,o+=4)r=n[c]<<16|n[c+1]<<8|n[c+2],a[o]=i[r>>18],a[o+1]=i[r>>12&63],a[o+2]=i[r>>6&63],a[o+3]=i[63&r];return t%3===1?(r=n[t-1],a[o]=i[r>>2],a[o+1]=i[r<<4&63],a[o+2]=61,a[o+3]=61):t%3===2&&(r=(n[t-2]<<8)+n[t-1],a[o]=i[r>>10],a[o+1]=i[r>>4&63],a[o+2]=i[r<<2&63],a[o+3]=61),new TextDecoder('ascii').decode(a)}; var xhr = new XMLHttpRequest(); xhr.responseType = 'arraybuffer'; xhr.onload = function(){ callback(toBase64(xhr.response)) }; xhr.onerror = function(){ callback(xhr.status) }; xhr.open('GET', uri); xhr.send();",
+            self.getDriver().current_url
+        )
+        if type(response) == int:
+            self.getLogger().error(
+                f"Request failed with HTTP Status {response}"
+            )
+            raise Exception(f"Request failed with HTTP status {response}")
+        return base64.b64decode(str(response))
 
     def scrapeDocumentFile(self, delay: float, company_detail: CompanyDetails) -> None:
         """
