f402614500145b02c8d5cf2f5d04eb8e7211d602
DONALDBZR <andygaspard@hotmail.com>
2024-06-25 15:17:42 +0400

    [UPDATE 2.0.24] The logger that will all the action of the application.

--- Models/DocumentReader.py
+++ Models/DocumentReader.py
@@ -9,6 +9,9 @@ Authors:
 """
 
 
+from Models.Logger import Corporate_Database_Builder_Logger
+
+
 class Document_Reader:
     """
     The model needed to generate the portable document file
@@ -16,3 +19,13 @@ class Document_Reader:
     data from it before deleting it from the cache of the server
     of the application.
     """
+    __logger: Corporate_Database_Builder_Logger
+    """
+    The logger that will all the action of the application.
+    """
+
+    def getLogger(self) -> Corporate_Database_Builder_Logger:
+        return self.__logger
+
+    def setLogger(self, logger: Corporate_Database_Builder_Logger) -> None:
+        self.__logger = logger
