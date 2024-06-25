9e77585f182f373e632b21823ebf89b2ba494989
DONALDBZR <andygaspard@hotmail.com>
2024-06-25 15:16:46 +0400

    [UPDATE 2.0.23] The module which will have the model needed to generate the portable document file version of the corporate registry, as well as extracting the data from it before deleting it from the cache of the server of the application.

---
+++ Models/DocumentReader.py
@@ -0,0 +1,18 @@
+"""
+The module which will have the model needed to generate the
+portable document file version of the corporate registry, as
+well as extracting the data from it before deleting it from
+the cache of the server of the application.
+
+Authors:
+    Andy Ewen Gaspard
+"""
+
+
+class Document_Reader:
+    """
+    The model needed to generate the portable document file
+    version of the corporate registry, as well as extracting the
+    data from it before deleting it from the cache of the server
+    of the application.
+    """
