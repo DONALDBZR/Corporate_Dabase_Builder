a6e4ebad9e97afb4dd357df0633f3b5e979bc968
DONALDBZR <andygaspard@hotmail.com>
2024-06-25 11:29:47 +0400

    [UPDATE 2.0.1] Adding the module needed for the extraction of the corporate data from the corporate documents.

---
+++ Auto/fincorp_module3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
+python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/extract_corporate_data.py
