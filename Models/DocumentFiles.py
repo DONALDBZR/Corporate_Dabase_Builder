72cffa1f946521c982f1a77a020d68fb58551099
DONALDBZR <andygaspard@hotmail.com>
2024-06-25 14:26:14 +0400

    [UPDATE 2.0.18] Retrieving the amount of corporate registries for a specific date of incorporation.

--- Models/DocumentFiles.py
+++ Models/DocumentFiles.py
@@ -142,3 +142,34 @@ class Document_Files(Database_Handler):
             "status": status,
             "data": data
         }
+
+    def getAmount(self, date_incorporation: str) -> int:
+        """
+        Retrieving the amount of corporate registries for a specific
+        date of incorporation.
+
+        Parameters:
+            date_incorporation: string: The date at which the company was legally formed.
+
+        Returns:
+            int
+        """
+        try:
+            parameters: Tuple[str] = (date_incorporation,)
+            data: Union[List[RowType], List[Dict[str, int]]] = self.getData(
+                table_name=self.getTableName(),
+                parameters=parameters,
+                join_condition=f"CompanyDetails ON {self.getTableName()}.CompanyDetail = CompanyDetails.identifier",
+                filter_condition="DATE(FROM_UNIXTIME(CompanyDetails.date_incorporation)) = %s",
+                column_names=f"COUNT({self.getTableName()}.identifier) AS amount_found"
+            )
+            status: int = self.getAmountDownloadedCorporateDocumentsStatus(data)
+            self.getLogger().inform(
+                f"The data from {self.getTableName()} has been retrieved!\nStatus: {status}\nData: {data}"
+            )
+            return int(data[0]["amount_found"]) # type: ignore
+        except Error as error:
+            self.getLogger().error(
+                f"An error occurred in {self.getTableName()}\nStatus: 503\nError: {error}"
+            )
+            return 0
