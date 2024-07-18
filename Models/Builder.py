bc0ea0e4f4a65c2a4fa9c9ea2eeb37d6ea35bd59
DONALDBZR <andygaspard@hotmail.com>
2024-07-18 15:47:53 +0400

    [UPDATE 2.4.11] Storing the corporate data that is extracted from the corporate registry.

--- Models/Builder.py
+++ Models/Builder.py
@@ -415,6 +415,8 @@ class Builder:
             response = self.storeCorporateDataAuthorisedCompany(dataset, document_file) # type: ignore
         elif company_detail.category.upper() == "GLOBAL BUSINESS COMPANY":
             response = self.storeCorporateDataGlobalBusinessCompany(dataset, document_file) # type: ignore
+        elif company_detail.category.upper() == "FOREIGN(DOM BRANCH)":
+            response = self.storeCorporateDataForeignDomestic(dataset, document_file) # type: ignore
         else:
             self.getLogger().error(f"The application will abort the extraction as the function has not been implemented!\nStatus: 503\nFunction: Builder.storeCorporateData()\nCategory: {company_detail.category}")
             exit()
