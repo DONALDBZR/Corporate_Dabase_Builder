c5c6412ec44f1eb9468c6b82b266c500da33a82c
DONALDBZR <andygaspard@hotmail.com>
2024-12-12 12:20:19 +0400

    [UPDATE 3.0.48] Sanitizing the nature where they are general retailers.

--- Models/Builder.py
+++ Models/Builder.py
@@ -2235,7 +2235,7 @@ class Builder:
         Returns:
             void
         """
-        general_retailers: List[BusinessDetails] = [business_detail for business_detail in self.getBusinessDetailsData() if business_detail.nature != None and "General Retailer" in business_detail.nature]
+        general_retailers: List[BusinessDetails] = [business_detail for business_detail in self.getBusinessDetailsData() if business_detail.nature != None and ("General Retailer" in business_detail.nature or "Foodstuff" in business_detail.nature)]
         filtered_business_details: List[BusinessDetails] = [business_detail for business_detail in self.getBusinessDetailsData()if business_detail not in general_retailers]
         self.setBusinessDetailsData([])
         self.getLogger().inform(f"Business Details: Nature: Sanitizing the nature where they are general retailers.\nAmount: {len(general_retailers)}")
