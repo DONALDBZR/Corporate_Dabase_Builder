4c2008a35ccaadb39367fa6cf45456c52f795c3e
DONALDBZR <andygaspard@hotmail.com>
2024-12-16 19:52:42 +0400

    [UPDATE 4.0.2] The Data Transfer Object for the State Capital.

---
+++ Data/StateCapital.py
@@ -0,0 +1,42 @@
+"""
+The Data Transfer Object for the State Capital.
+
+Authors:
+    Darkness4869
+"""
+
+
+from dataclasses import dataclass
+from mysql.connector.types import RowType
+from typing import Union, Dict
+
+
+@dataclass
+class StateCapital:
+    """
+    The Data Transfer Object for the State Capital.
+    """
+    identifier: int
+    CompanyDetail: int
+    type: Union[str, None]
+    amount: Union[int, None]
+    state_capital: Union[float, None]
+    amount_unpaid: Union[float, None]
+    currency: Union[str, None]
+    def __init__(self, dataset: Union[RowType, Dict[str, Union[int, str, float, None]]]) -> None:
+        """
+        Initializing the data class object.
+
+        Parameters:
+            dataset: {identifier: int, CompanyDetail: int, type: string|null, amount: int|null, state_capital: float|null, amount_unpaid: float|null, currency: string|null}: The result set from the relational database server.
+
+        Returns:
+            void
+        """
+        self.identifier = int(dataset["identifier"]) # type: ignore
+        self.CompanyDetail = int(dataset["CompanyDetail"]) # type: ignore
+        self.type = dataset["type"] # type: ignore
+        self.amount = dataset["amount"] # type: ignore
+        self.state_capital = dataset["state_capital"] # type: ignore
+        self.amount_unpaid = dataset["amount_unpaid"] # type: ignore
+        self.currency = dataset["currency"] # type: ignore
