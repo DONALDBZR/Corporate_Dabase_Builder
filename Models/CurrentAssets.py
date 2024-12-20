62fda7f30a2bfe4d9c93d5540fc44b5b22ff4283
DONALDBZR <andygaspard@hotmail.com>
2024-12-20 19:41:43 +0400

    [UPDATE 5.5.1] The model which will interact exclusively with the Current Assets table.

---
+++ Models/CurrentAssets.py
@@ -0,0 +1,39 @@
+"""
+The model which will interact exclusively with the Current
+Assets table.
+
+Authors:
+    Darkness4869
+"""
+
+
+from Models.DatabaseHandler import Database_Handler
+from typing import Dict, Union, Tuple
+from mysql.connector.errors import Error
+
+
+class Non_Current_Assets(Database_Handler):
+    """
+    The model which will interact exclusively with the Current
+    Assets table.
+    """
+    __table_name: str
+    """
+    The table which the model is linked to.
+    """
+    created: int = 201
+    """
+    The status code for a successful creation.
+    """
+    service_unavailable: int = 503
+    """
+    The status code for an unavailable service.
+    """
+    ok: int = 200
+    """
+    The status code for a successful response
+    """
+    no_content: int = 204
+    """
+    The status code when there is no content.
+    """
