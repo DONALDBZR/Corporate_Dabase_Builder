7960395d3af22385a92491736895e1a188729681
DONALDBZR <andygaspard@hotmail.com>
2024-12-20 21:02:29 +0400

    [UPDATE 5.7.1] The model which will interact exclusively with the Equity And Liabilities table.

---
+++ Models/EquityAndLiabilities.py
@@ -0,0 +1,47 @@
+"""
+The model which will interact exclusively with the Equity
+And Liabilities table.
+
+Authors:
+    Darkness4869
+"""
+
+
+from Models.DatabaseHandler import Database_Handler
+from typing import Dict, Union, Tuple, List
+from mysql.connector.errors import Error
+from mysql.connector.types import RowType
+from Data.Liabilities import Liabilities as data_object
+
+
+class Equity_And_Liabilities(Database_Handler):
+    """
+    The model which will interact exclusively with the Equity
+    And Liabilities table.
+    """
+    __table_name: str
+    """
+    The table which the model is linked to.
+    """
+    created: int = 201
+    """
+    The status code for a successful creation.
+    """
+    service_unavailable: int = 503
+    """
+    The status code for an unavailable service.
+    """
+    ok: int = 200
+    """
+    The status code for a successful response
+    """
+    no_content: int = 204
+    """
+    The status code when there is no content.
+    """
+
+    def getTableName(self) -> str:
+        return self.__table_name
+
+    def setTableName(self, table_name: str) -> None:
+        self.__table_name = table_name
