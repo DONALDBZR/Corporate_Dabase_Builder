d95dd968941d8849c54fa129cec077cefaef424e
DONALDBZR <andygaspard@hotmail.com>
2024-12-20 18:43:21 +0400

    [UPDATE 5.0.53] The model which will interact exclusively with the Assets table.

---
+++ Models/Assets.py
@@ -0,0 +1,47 @@
+"""
+The model which will interact exclusively with the Assets
+table.
+
+Authors:
+    Darkness4869
+"""
+
+
+from Data.Assets import Assets
+from Models.DatabaseHandler import Database_Handler
+from typing import Dict, Union, Tuple, List
+from mysql.connector.errors import Error
+from mysql.connector.types import RowType
+
+
+class Assets(Database_Handler):
+    """
+    The model which will interact exclusively with the Assets
+    table.
+    """
+    __table_name: str
+    """
+    The table which the model is linked to.
+    """
+    created: int = 201
+    """
+    The status code for a successful creation.
+    """
+    service_unavailable: int = 503
+    """
+    The status code for an unavailable service.
+    """
+    ok: int = 200
+    """
+    The status code for a successful response
+    """
+    no_content: int = 204
+    """
+    The status code when there is no content.
+    """
+
+    def getTableName(self) -> str:
+        return self.__table_name
+
+    def setTableName(self, table_name: str) -> None:
+        self.__table_name = table_name
