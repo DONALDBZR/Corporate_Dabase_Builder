981c37c65f1e89bc3d1a8924813418d1be0e2bab
DONALDBZR <andygaspard@hotmail.com>
2024-12-20 20:47:51 +0400

    [UPDATE 5.6.4] Adding the balance sheet of the company.

--- Models/Liabilities.py
+++ Models/Liabilities.py
@@ -52,3 +52,28 @@ class Liabilities(Database_Handler):
 
     def setTableName(self, table_name: str) -> None:
         self.__table_name = table_name
+
+    def addAsset(self, data: Dict[str, Union[Dict[str, float], float]], balance_sheet: int) -> int:
+        """
+        Adding the balance sheet of the company.
+
+        Parameters:
+            data: {equity_and_liabilities: {share_capital: float, other_reserves: float, retained_earnings: float, others: float, total: float}, non_current: {long_term_borrowings: float, deferred_tax: float, long_term_provisions: float, others: float, total: float}, current: {trade: float, short_term_borrowings: float, current_tax_payable: float, short_term_provisions: float, others: float, total: float}, total_liabilities: float, total_equity_and_liabilities: float}: The data of the assets.
+            balance_sheet: int: The identifier of a balance sheet.
+
+        Returns:
+            int
+        """
+        try:
+            parameters: Tuple[int, float, float] = (balance_sheet, float(data["total_liabilities"]), float(data["total_equity_and_liabilities"])) # type: ignore
+            self.postData(
+                table=self.getTableName(),
+                columns="BalanceSheet, total_liabilities, total_equity_and_liabilities",
+                values="%s, %s, %s",
+                parameters=parameters # type: ignore
+            )
+            self.getLogger().inform(f"The data has been successfully stored.\nStatus: {self.created}")
+            return self.created
+        except Error as error:
+            self.getLogger().error(f"An error occurred in {self.getTableName()}\nStatus: {self.service_unavailable}\nError: {error}")
+            return self.service_unavailable
