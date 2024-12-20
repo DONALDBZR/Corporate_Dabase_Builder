d72cd3208e3c9469e7cc01e5e6ca56a8df7e8dd5
DONALDBZR <andygaspard@hotmail.com>
2024-12-20 18:10:01 +0400

    [UPDATE 5.0.47] Initializing the data class object.

--- Data/BalanceSheet.py
+++ Data/BalanceSheet.py
@@ -20,3 +20,16 @@ class FinancialSummaries:
     financial_year: int
     currency: str
     unit: int
+
+    def __init__(self, dataset: Union[RowType, Dict[str, Union[int, str]]]):
+        """
+        Initializing the data class object.
+
+        Parameters:
+            dataset: {identifier: int, CompanyDetail: int, financial_year: int, currency: string, unit: int}: The result set from the relational database server.
+        """
+        self.identifier = int(dataset["identifier"]) # type: ignore
+        self.CompanyDetail = int(dataset["CompanyDetail"]) # type: ignore
+        self.financial_year = int(dataset["financial_year"]) # type: ignore
+        self.currency = str(dataset["currency"]) # type: ignore
+        self.unit = int(dataset["unit"]) # type: ignore
