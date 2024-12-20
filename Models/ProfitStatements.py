5aef11d1099d99c1c656a895e14afa08b175dec5
DONALDBZR <andygaspard@hotmail.com>
2024-12-20 14:28:44 +0400

    [UPDATE 5.0.40] Initializing all of the dependencies which will be used to operate the application.

--- Models/ProfitStatements.py
+++ Models/ProfitStatements.py
@@ -30,6 +30,15 @@ class Profit_Statements(Database_Handler):
     The status code for an unavailable service.
     """
 
+    def __init__(self):
+        """
+        Initializing all of the dependencies which will be used to
+        operate the application.
+        """
+        super().__init__()
+        self.setTableName("ProfitStatements")
+        self.getLogger().inform("The model has been successfully been initiated with its dependencies.")
+
     def getTableName(self) -> str:
         return self.__table_name
 
