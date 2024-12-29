7177a14a30799a3a02d390da911c2b36fafdf0f3
DONALDBZR <andygaspard@hotmail.com>
2024-12-29 14:00:59 +0400

    [UPDATE 5.16.1] Allowing the second module to run on a virtual display server.

--- Auto/fincorp_module2.sh
+++ Auto/fincorp_module2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+Xvfb :99 -screen 0 1920x1080x24 &
+export DISPLAY=:99
 source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
 python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/download_corporate_file.py
+killall Xvfb
