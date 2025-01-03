62ba350581bb5b2d132e85b79a2d90791c2b7bad
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 12:56:05 +0400

    [UPDATE 5.20.4] The model which will interact exclusively with the Annual Returns table.

---
+++ Models/AnnualReturns.py
@@ -0,0 +1,54 @@
+"""
+The model which will interact exclusively with the Annual
+Returns table.
+
+Authors:
+    Darkness4869
+"""
+
+
+from Models.DatabaseHandler import Database_Handler
+from typing import Dict, Union, Tuple
+from mysql.connector.errors import Error
+
+
+class Annual_Returns(Database_Handler):
+    """
+    The model which will interact exclusively with the Annual
+    Returns table.
+    """
+    __table_name: str
+    """
+    The table which the model is linked to.
+    """
+    created: int = 201
+    """
+    The status code for a successful creation.
+    """
+    service_unavailable: int = 503
+    """
+    The status code for an unavailable service.
+    """
+    ok: int = 200
+    """
+    The status code for a successful response
+    """
+    no_content: int = 204
+    """
+    The status code when there is no content.
+    """
+
+    def __init__(self):
+        """
+        Initializing all of the dependencies which will be used to
+        operate the application.
+        """
+        super().__init__()
+        self.setTableName("AnnualReturns")
+        self.getLogger().inform("The model has been successfully been initiated with its dependencies.")
+
+    def getTableName(self) -> str:
+        return self.__table_name
+
+    def setTableName(self, table_name: str) -> None:
+        self.__table_name = table_name
