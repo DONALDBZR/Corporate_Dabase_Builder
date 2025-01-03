fd0c24c764571cdb34ccb8a3f77ebbec5a6abae2
DONALDBZR <andygaspard@hotmail.com>
2025-01-03 13:05:06 +0400

    [UPDATE 5.20.5] Adding the annual return of the company.

--- Models/AnnualReturns.py
+++ Models/AnnualReturns.py
@@ -52,3 +52,28 @@ class Annual_Returns(Database_Handler):
 
     def setTableName(self, table_name: str) -> None:
         self.__table_name = table_name
+
+    def addAnnualReturn(self, data: Dict[str, int], company_detail: int) -> int:
+        """
+        Adding the annual return of the company.
+
+        Parameters:
+            data: {date_annual_return: int, date_annual_meeting: int, date_filled: int}: The data of the annual return
+            company_detail: int: The identifier of a company
+
+        Returns:
+            int
+        """
+        try:
+            parameters: Tuple[int, int, int, int] = (company_detail, data["date_annual_return"], data["date_annual_meeting"], data["date_filled"])
+            self.postData(
+                table=self.getTableName(),
+                columns="CompanyDetail, date_annual_return, date_annual_meeting, date_filled",
+                values="%s, %s, %s, %s",
+                parameters=parameters # type: ignore
+            )
+            self.getLogger().inform(f"The data has been successfully stored.\nStatus: {self.created}")
+            return self.created
+        except Error as error:
+            self.getLogger().error(f"An error occurred in {self.getTableName()}\nStatus: {self.service_unavailable}\nError: {error}")
+            return self.service_unavailable
