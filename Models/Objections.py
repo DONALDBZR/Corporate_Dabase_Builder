c93df1c063269a75334b00ae5608c2362e1dea0b
DONALDBZR <andygaspard@hotmail.com>
2025-01-08 15:25:46 +0400

    [UPDATE 5.21.3] Adding the objection of the company.

--- Models/Objections.py
+++ Models/Objections.py
@@ -23,6 +23,14 @@ class Objections(Database_Handler):
     """
     The table which the model is linked to.
     """
+    created: int = 201
+    """
+    The status code for a success write.
+    """
+    service_unavailable: int = 503
+    """
+    The status code for unavailability of service.
+    """
 
     def __init__(self) -> None:
         """
@@ -38,3 +46,28 @@ class Objections(Database_Handler):
 
     def setTableName(self, table_name: str) -> None:
         self.__table_name = table_name
+
+    def addObjection(self, data: Dict[str, Union[int, str]], company_detail: int) -> int:
+        """
+        Adding the objection of the company.
+
+        Parameters:
+            data: [{date_objection: int, objector: string}]: The data of the objection.
+            company_detail: int: The identifier of a company.
+
+        Returns:
+            int
+        """
+        try:
+            parameters: Tuple[int, int, str] = (company_detail, int(str(data["date_objection"])), str(data["objector"]))
+            self.postData(
+                table=self.getTableName(),
+                columns="CompanyDetail, date_objection, objector",
+                values="%s, %s, %s",
+                parameters=parameters # type: ignore
+            )
+            self.getLogger().inform(f"The data has been successfully stored.\nStatus: {self.created}")
+            return self.created
+        except Error as error:
+            self.getLogger().error(f"An error occurred in {self.getTableName()}\nStatus: {self.service_unavailable}\nError: {error}")
+            return self.service_unavailable
