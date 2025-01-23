820f6079b7d43f1c9cdc42a81630acd3e7bbb723
DONALDBZR <andygaspard@hotmail.com>
2025-01-23 19:02:06 +0400

    [UPDATE 6.0.1] The main bash script for the curation of the shareholders

--- Auto/fincorp.data_curation.sh
+++ Auto/fincorp.data_curation.sh
@@ -2,3 +2,4 @@
 sh /home/darkness4869/Documents/Corporate_Database_Builder/Auto/fincorp_module4.sh
 sh /home/darkness4869/Documents/Corporate_Database_Builder/Auto/fincorp_module5.sh
 sh /home/darkness4869/Documents/Corporate_Database_Builder/Auto/fincorp_module6.sh
+sh /home/darkness4869/Documents/Corporate_Database_Builder/Auto/fincorp_module7.sh
