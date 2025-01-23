724b11601642281caefd149d0bf6ebe2a0e9e9ad
DONALDBZR <andygaspard@hotmail.com>
2025-01-23 22:19:49 +0400

    [UPDATE 7.0.2] The main bash script for the data curation of the members of a company.

---
+++ Auto/fincorp_module8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm /home/darkness4869/Documents/Corporate_Database_Builder/Logs/CDB.log
+source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
+python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/curate_members.py
