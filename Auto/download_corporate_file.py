3c63270acc6865eddccb02c94edaf85d614a630c
DONALDBZR <andygaspard@hotmail.com>
2025-02-07 03:42:04 +0400

    [UPDATE 8.5.4] Adjusting the execution time of the second module

--- Auto/download_corporate_file.py
+++ Auto/download_corporate_file.py
@@ -9,7 +9,7 @@ path.insert(0, "/home/darkness4869/Documents/Corporate_Database_Builder")
 from Models.Builder import Builder
 
 
-module_limit_execution = datetime.now() + timedelta(seconds=22508.425607843)
+module_limit_execution = datetime.now() + timedelta(seconds=14199.635555556)
 module_limit_execution_time = module_limit_execution.timestamp()
 while datetime.now().timestamp() < module_limit_execution_time:
     Corporate_Database_Builder = Builder()
