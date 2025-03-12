1ddbe2af32aecd176870368bd00374fcafb7d293
DONALDBZR <andygaspard@hotmail.com>
2025-03-12 15:37:05 +0400

    [UPDATE 8.22.1] Adjustment of the script

--- Auto/fincorp_module1.sh
+++ Auto/fincorp_module1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm /home/darkness4869/Documents/Corporate_Database_Builder/Logs/CDB.log
 Xvfb :99 -screen 0 1920x1080x24 &
 export DISPLAY=:99
 source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
