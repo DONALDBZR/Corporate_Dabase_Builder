0ad2171df90e1c18f17ad1876a0fff09a08d2764
DONALDBZR <andygaspard@hotmail.com>
2025-03-16 00:41:48 +0400

    [UPDATE 8.28.1] Adjusting the script

--- Auto/fincorp_module8.sh
+++ Auto/fincorp_module8.sh
@@ -2,3 +2,4 @@
 rm /home/darkness4869/Documents/Corporate_Database_Builder/Logs/CDB.log
 source /home/darkness4869/Documents/Corporate_Database_Builder/venv/bin/activate
 python3 /home/darkness4869/Documents/Corporate_Database_Builder/Auto/curate_members.py
+deactivate
